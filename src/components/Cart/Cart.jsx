39847c7c2c2179dcfb11d060d47e79724812e3bc
Chung-Ying <ag.cyho@gmail.com>
2023-07-17 23:50:31 +0800

    feat: add props to calculate total price and amount

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,22 @@
 
-import Item from "./Item/Item"
+import ItemList from "./Item/ItemList"
 import styles from './Cart.module.css'
+import { useState } from "react"
 
 export default function Cart(){
+    const [total, setTotal] = useState(0)
     return(
         <section className={styles.container}>
             <div className={styles.cart_container}>
                 <h3>購物籃</h3>
-                <Item />
+                <ItemList setTotal={setTotal}/>
                 <div className={styles.price}>
                     <p><b>運費</b></p>
                     <p>免費</p>
                 </div>
                 <div className={styles.total}>
                     <p><b>小計</b></p>
-                    <p>$300</p>
+                    <p>${total}</p>
                 </div>
             </div>
         </section>
---
+++ src/components/Cart/Item/ItemList.jsx
@@ -0,0 +1,44 @@
+import items from './items'
+import styles from './ItemList.module.css'
+import {ReactComponent as Minus} from '../../../icons/minus.svg'
+import {ReactComponent as Plus} from '../../../icons/plus.svg'
+import { useState } from 'react'
+
+
+export function Item({item, setTotal}){
+    const [count, setCount] = useState(0)
+    const minusCount = ()=>{
+        setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : prevCount))
+        setTotal(prevTotal => prevTotal - item.price)
+    }
+    const plusCount = ()=>{
+        setCount((prevCount) => prevCount + 1)
+        setTotal(prevTotal => prevTotal + item.price)
+    }
+    
+    return(
+        <div className={styles.product_container} key={item.id}>
+            <img src={item.img} alt={item.name} className={styles.img} />
+            <div className={styles.product_info}>
+                <div className={styles.left_area}>
+                    <span className={styles.product_name}>{item.name}</span>
+                    <div className={styles.product_controller}>
+                        <Minus onClick={minusCount}/>
+                        <span className={styles.product_count}>{count}</span>
+                        <Plus onClick={plusCount}/>
+                    </div>
+                </div>
+                <span className={styles.price}>${item.price * count}</span>
+            </div>
+        </div>
+    )
+}
+
+export default function ItemList({setTotal}){
+    return(
+        <section>
+            {items.map(item =>
+            <Item item={item} key={item.id} setTotal={setTotal}/>)}
+        </section>
+    )
+}
