5792484522fcb187f9000e5148f1d7c00eeddffe
Chung-Ying <ag.cyho@gmail.com>
2023-07-20 15:54:44 +0800

    feat: modified step3 style.

--- src/components/Step/Step3/Step3.jsx
+++ src/components/Step/Step3/Step3.jsx
@@ -5,25 +5,24 @@ export default function Step3(){
         <section className={styles.container}>
             <h3 className={styles.title}>付款資訊</h3>
             <form className={styles.form}>
-                <div className={styles.group}>
+                <div className={`${styles.group} ${styles.name}`}>
                     <div>持卡人姓名</div>
-                    <input type="text" placeholder="John Doe" />
+                    <input type="text" placeholder="John Doe" className={styles.input}/>
                 </div>
 
-                <div className={styles.group}>
+                <div className={`${styles.group} ${styles.card_number}`}>
                     <div>卡號</div>
-                    <input type="text" placeholder="1111 2222 3333 4444" />
+                    <input type="text" placeholder="1111 2222 3333 4444" className={styles.input}/>
                 </div>
-                <div className={styles.card_info}>
-                    <div claName={styles.group}>
-                        <div>有效期限</div>
-                        <input type="text" placeholder="MM/YY" />
-                    </div>
 
-                    <div claName={styles.group}>
-                        <div>CVC / CCV</div>
-                        <input type="text" placeholder="123" />
-                    </div>
+                 <div className={`${styles.group} ${styles.dates}`}>
+                    <div>有效期限</div>
+                    <input type="text" placeholder="MM/YY" className={styles.input}/>
+                </div>
+
+                <div className={`${styles.group} ${styles.ccc}`}>
+                    <div>CVC / CCV</div>
+                    <input type="text" placeholder="123" className={styles.input}/>
                 </div>
             </form>
         </section>
