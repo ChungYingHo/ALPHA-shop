7edf2b542c8a19163bbd48c3ef3fa59860c4f61d
Chung-Ying <ag.cyho@gmail.com>
2023-07-21 16:57:51 +0800

    add cursor: pointer to minus and plus components

--- src/components/Cart/Item/Item.jsx
+++ src/components/Cart/Item/Item.jsx
@@ -20,9 +20,9 @@ export default function Item({item, setCart, cart}){
                     <span className={styles.product_name}>{item.name}</span>
                     <div className={styles.product_controller}>
                         {/* 商品數量大於0才可以減1 */}
-                        <Minus onClick={()=>item.quantity > 0 && handleUpdated(-1)}/>
+                        <Minus className={styles.btn} onClick={()=>item.quantity > 0 && handleUpdated(-1)}/>
                         <span className={styles.product_count}>{item.quantity}</span>
-                        <Plus onClick={()=>handleUpdated(1)}/>
+                        <Plus className={styles.btn} onClick={()=>handleUpdated(1)}/>
                     </div>
                 </div>
                 <span className={styles.price}>${item.price * item.quantity}</span>
